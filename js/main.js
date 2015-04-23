a6bc89be5c2c94ca4e62cca4d08abfde4e149cf7
narcher2 <narcher2@gmu.edu>
2015-04-23 11:14:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,30 +21,34 @@ function create() {
     music.play();
     
     step = 0;
+    anger = 0;
+    happiness = 0;
+    disgust = 0;
+    sadness = 0;
 
     game.stage.backgroundColor = '#182d3b';
 
     background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-    button = game.add.button(32, 400, 'button', actionOnClick, this, 2, 1, 0);
+    button = game.add.button(32, 400, 'button', actionOnClick1, this, 2, 1, 0);
 
     button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
     button.onInputUp.add(up, this);
     
-    button2 = game.add.button(32, 448, 'button', actionOnClick, this, 2, 1, 0);
+    button2 = game.add.button(32, 448, 'button', actionOnClick1, this, 2, 1, 0);
 
     button2.onInputOver.add(over, this);
     button2.onInputOut.add(out, this);
     button2.onInputUp.add(up, this);
     
-    button3 = game.add.button(32, 496, 'button', actionOnClick, this, 2, 1, 0);
+    button3 = game.add.button(32, 496, 'button', actionOnClick1, this, 2, 1, 0);
 
     button3.onInputOver.add(over, this);
     button3.onInputOut.add(out, this);
     button3.onInputUp.add(up, this);
     
-    button4 = game.add.button(32, 540, 'button', actionOnClick, this, 2, 1, 0);
+    button4 = game.add.button(32, 540, 'button', actionOnClick1, this, 2, 1, 0);
 
     button4.onInputOver.add(over, this);
     button4.onInputOut.add(out, this);
@@ -121,7 +125,7 @@ function updateText() {
 
 }
 
-function actionOnClick () {
+function actionOnClick1 () {
     
     step++;
 
